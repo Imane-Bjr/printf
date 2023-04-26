7e17d213f593c9af6268b0bb083722b94209c098
houzila <125467030+houzila@users.noreply.github.com>
2023-04-26 04:59:12 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -25,6 +25,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
@@ -46,3 +47,4 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
+

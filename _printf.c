5c9cd368b84d03bb966cc1a4bdfe89f7bcfb1976
houzila <125467030+houzila@users.noreply.github.com>
2023-04-26 05:04:41 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
-
 void print_buffer(char buffer[], int *buff_ind);
 
-
 /**
- *  _printf - Printf function
- *  @format: format .
- *  Return: Printed chars.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-int _printf(consta char *format, ...)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -40,6 +38,7 @@ int _printf(consta char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -65,3 +64,5 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
+
+

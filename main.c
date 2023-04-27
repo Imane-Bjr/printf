fff14b31f415deea1b63d8a97a9fed4802452c1d
Imane <i.houzila@mundipolis.ma>
2023-04-27 15:53:58 -0700

    please work

--- main.c
+++ main.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-	int len, len2;
+    int len, len2;
 
-	len = _printf("%6d", 102498402);
-	len2 = printf("%6d", 102498402);
-	fflush(stdout);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
-	return (0);
+    len = _printf("%6d", -1024);
+    len2 = printf("%6d\n", -1024);
+    fflush(stdout);
+    if (len != len2)
+    {
+        printf("Lengths differ.\n");
+        fflush(stdout);
+        return (1);
+    }
+    return (0);
 }

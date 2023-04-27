f1cb5bae5a85eb420c62fae90b1cb80be5ebff1c
Imane-Bjr <61564697+Imane-Bjr@users.noreply.github.com>
2023-04-27 21:42:35 +0000

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -6,11 +6,10 @@
  * @i: take a parameter.
  * Return: Flags:
  */
-
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
+	/* 1 2 4 8 16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -24,9 +23,11 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
+
 	*i = curr_i - 1;
 
 	return (flags);

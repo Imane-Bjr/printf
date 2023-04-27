fbae89b46cd0f4c74c831d396ed017dd0dea4749
houzila <125467030+houzila@users.noreply.github.com>
2023-04-28 00:47:38 +0200

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
@@ -16,7 +18,7 @@ int get_width(const char *format, int *i, va_list list)
 		if (is_digit(format[curr_i]))
 		{
 			width *= 10;
-			width += format[curr_i] - '\0';
+			width += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
@@ -27,7 +29,8 @@ int get_width(const char *format, int *i, va_list list)
 		else
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (width);
 }
-
